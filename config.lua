532db0bad29d8b1fb487369c327bbc94eb0a52dd
Zeckone <48627324+Zeckone01@users.noreply.github.com>
2022-09-19 09:49:34 +0700

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,15 @@
+Apartments = {}
+
+Apartments.Starting = true
+
+Apartments.SpawnOffset = 30
+
+Apartments.Locations = {
+    ["apartment1"] = {
+        name = "apartment1",
+        label = "Alfa Street",
+        coords = {
+            enter = vector4(-270.98, -957.76, 31.23, 118.03),
+        }
+    },
+}
